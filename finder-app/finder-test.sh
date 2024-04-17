ecd47b8ae1adf20ab568d6c9d1cc2200a9809ab1
Charlez Kwan <charlezkwan@gmail.com>
2024-04-17 21:32:06 +0200

    added dirname bash for running writer / finder

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -13,7 +13,7 @@ NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
 username=$(cat /etc/finder-app/conf/username.txt)
-
+SCRIPTDIR=`dirname $0`
 if [ $# -lt 3 ]
 then
 	echo "Using default value ${WRITESTR} for string to write"
@@ -58,10 +58,10 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	SCRIPTDIR/writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(SCRIPTDIR/finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
